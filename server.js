4d3c768d64928130026da2d89a75d89908225366
Fritz <fritz.developer10@gmail.com>
2023-10-11 07:02:16 -0500

    ajuste de estilo en body

--- server.js
+++ server.js
@@ -67,161 +67,108 @@ sendMail({
   email: "disenador.uxui@mablovers.com",
   subject: "Hello",
   text: "Hello world",
-  html: `
-  <!DOCTYPE html>
-  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-  <html
-    xmlns="http://www.w3.org/1999/xhtml"
-    xmlns:o="urn:schemas-microsoft-com:office:office"
-    xmlns:v="urn:schemas-microsoft-com:vml"
-  >
-  <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      
-      
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
-  
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
-  
-      <title>kind</title>
-  </head>
-  <style type="text/css">
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-  
-  * {
-      box-sizing: border-box;
-      margin: 0;
-      padding: 0;
-      text-decoration: none;
-      list-style: none;
-      font-family: 'Montserrat', sans-serif;  
-  }
-  
-  html {
-      font-size: 62.5%;
-  }
-  .body{
-      background-color: #00B4C1;
-  }
-  
-  
-  header{
-      height: 200px;
-      height: 50%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-      margin-top: 50px;
-  }
-  
-  .logo{
-      width: 18%;
-      display: flex;
-      align-items: center;
-      justify-content: start;
-  }
-  
-  main{
-      margin-top: 20px;
-      margin-left: 10%;
-      margin-right: 10%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      font-size: 20px;
-      color:#F9F6F2 ;
-      gap: 10px;
-      
-  }
-  
-  h2{
-      font-family: 'Roboto Condensed', sans-serif; 
-      text-transform: uppercase; 
-  }
-  
-  a{
-      background: #DF1E44;
-      min-width: 200px;
-      cursor: pointer;
-      border: none;
-      padding: 16px 32px;
-      color: azure;
-      font-size: 24px;
-      font-weight: bold;
-      position: relative;
-      border-radius: 12px;
-      margin-top: 20px;
-      margin-bottom: 20px;
-  }
-  
-  span{
-      font-weight: bold;
-      font-size: 22px;
-      color: #DF1E44;
-  }
-  
-  a:hover::before {
-      content: "";
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: -1;
-      width: 100%;
-      height: 100%;
-      background: linear-gradient(45deg, white, rgb(104, 227, 251), white, rgb(104, 227, 251), white, rgb(104, 227, 251), white, rgb(68, 224, 255));
-      background-size: 800%;
-      border-radius: 12px;
-      filter: blur(8px);
-      animation: glowing 20s linear infinite;
-  }
-  
-  @keyframes glowing {
-      0% {
-          background-position: 0 0;
-      }
-      50% {
-          background-position: 400% 0;
-      }
-      100% {
-          background-position: 0 0;
-      }
-  }
-  
-  @media (max-width: 768px) {
-      main{
-          font-size: 18px;
-      }
-  }
-  
-  @media (max-width: 400px) {
-      main{
-          font-size: 16px;
-      }
-  }
-  </style>
-  <body>
-      <header>
-          <img class="logo" src="https://i.postimg.cc/xTHbPnkS/Logo-Kind-06.png" />
-      </header>
-      <main>
-          <h2>Hola, ${names}</h2>
-          <p>Te invitamos a registrarte en la plataforma 
-              Kind con tu correo ${email}.
-          </p>
-          <p>
-              Para que  puedas confirmar tu correo y generar tu contraseña ñeñas
-          </p>
-          <a href="${url}">Click aquí</a>
-          <p>
-              Si en caso no has podido generar tu contraseña comunicate <br> al correo:  <span>contacto@mablover.com </span>y te enviaremos un nuevo enlace.
-          </p>
-      </main>
-  </body>
-  </html>
-  `,
-}) */
+ html: `
+ <body>
+ <head>
+  <title>
+  </title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
 
+  <style>
+     * {
+         font-family: 'Montserrat', sans-serif;  
+        .container{
+            background-color: #00B4C1; 
+            text-align: center;
+        }
+        header{
+            padding: 40px;
+            height: 30%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+   
+        .logo{
+            width: 18%;
+            margin-top: 80px;
+        }
+   
+        .main{
+            margin-top: 20px;
+            margin-left: 10%;
+            justify-content: center;
+            align-items: center;
+            font-size: 20px;
+            color:#F9F6F2 ;
+       }
+   
+        h2{
+            margin-top: 50px;
+            font-family: 'Roboto Condensed', sans-serif; 
+            text-transform: uppercase; 
+        }
+        p{
+            color:white;
+            flex-direction: column;
+            gap: 10px;
+        }
+        .text{
+            margin: 20px;
+        }
+   
+        .ptb20{
+            padding-bottom: 20px;
+        }
+   
+        .ptt20{
+            padding-top: 20px;
+        }
+        .button{
+            background: #DF1E44;
+            min-width: 200px;
+            cursor: pointer;
+            border: none;
+            padding: 16px 32px;
+            color: azure;
+            font-size: 24px;
+            font-weight: bold;
+            position: relative;
+            border-radius: 12px;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+   
+        span{
+            font-weight: bold;
+            font-size: 16px;
+            color: #DF1E44;
+        }
+   
+     </style>
+    </head>
+    <table  class="container" role="presentation" style="width:100%;border:0;border-collapse:collapse;border-spacing:0;">
+        <tr class="header">
+            <td class="upcoming-promotion-text">
+                <img class="logo" src="https://i.postimg.cc/J0ztmw5T/Logo-Kind-07.png" />
+            </td>
+        </tr>
+        <tr class="main">
+            <td class="containerMain"  style="padding: 20px 0 90px 0; color:#EEFBFB; font-size: 12px;">
+                <h2 style=" font-family: Arial, sans-serif; text-decoration: none;" href="#">Hola, ${names}</h2>
+                <p class="text" style="font-family: Arial, sans-serif;">
+                    Te invitamos a registrarte en la plataforma </p> 
+                <p class="text" style="font-family: Arial, sans-serif;">Kind con tu correo ${email}. </p>
+                <p class="text ptb20" style="font-family: Arial, sans-serif;">Para que  puedas confirmar tu correo y generar tu contraseña has</p>
+                <a ref="${url}" class="button" style=" font-family: Arial, sans-serif; text-decoration: none;" >Click aquí</a>
+                <p class="text ptt20">Si en caso no has podido generar tu contraseña comunicate <br> al correo:  <span>contacto@mablover.com </span>y te enviaremos un nuevo enlace.</p>
+            </td>
+        </tr>
+    </table>
+   </body>
+    `,
+   }) */
