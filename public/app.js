dbb7c051152cce5a0cc2d9afcf0445d7ef458e89
Fritz <fritz.developer10@gmail.com>
2024-02-13 13:15:50 -0500

    actualizando landyng

--- public/app.js
+++ public/app.js
@@ -8,8 +8,9 @@ fetch('app.json')
         div.innerHTML = `
             <img class="logoSlogan" src="${item.imgSrc}" alt="">
             <div class="itemsOne">
-                <h2>${item.h2}</h2>
+                <h1>${item.h1}</h1>
                 <p>${item.p}</p>
+                <button>${item.button}</button>
             </div>
         `;
         oneSection.appendChild(div);
@@ -19,15 +20,15 @@ fetch('app.json')
         data.twoSection.forEach(item => {
         const div = document.createElement('div');
         div.innerHTML = `
+        <h2 class="t-blueKumpai t-48 t-center">${item.title}</h2>
             <div class="twoBoxs  efects">
-                <div class="sTBoxOne bg-blueKumpaiLigth border-box card">
+                <div class="sTBoxOne bg-white border-box card">
+                    <img  src="${item.icon1}" alt="">
                     <p>${item.p1}</p>
                 </div>
                 <div class="sTBoxtwo">
                     <img class="phoneCircle card" src="${item.imgSrc1}" alt="">
-                    <div class="blueCircle">
-                        <span class="litleCircle"></span>
-                    </div>
+
                 </div>
             </div>
             <div class="twoBoxs efects">
@@ -35,8 +36,28 @@ fetch('app.json')
                     <img class= "desktopCircle" src="${item.imgSrc2}" alt="">
                     <div class="orangeCircle"></div>
                 </div>
-                <div class="sTBoxtwo bg-orangeKumpaiLigth border-box">
+                <div class="sTBoxtwo bg-white border-box">
+                    <img  src="${item.icon2}" alt="">
                     <p>${item.p2}</p>
+                </div>
+            </div>
+            <div class="twoBoxs  efects">
+                <div class="sTBoxOne bg-white border-box card">
+                    <img  src="${item.icon3}" alt="">
+                    <p>${item.p3}</p>
+                </div>
+                <div class="sTBoxtwo">
+                    <img class="phoneCircle card" src="${item.imgSrc3}" alt="">
+                </div>
+            </div>
+            <div class="twoBoxs efects">
+                <div class="sTBoxOne">
+                    <img class= "desktopCircle" src="${item.imgSrc4}" alt="">
+
+                </div>
+                <div class="sTBoxtwo bg-white border-box">
+                    <img  src="${item.icon4}" alt="">
+                    <p>${item.p4}</p>
                 </div>
             </div>
         `;
@@ -46,60 +67,71 @@ fetch('app.json')
         //section three
         const threeSection = document.querySelector('.threeSection');
         data.threeSection.forEach(item => {
-        const cajaCero = document.createElement('div');
-        cajaCero.innerHTML =`
-                    <h2 class="efects">${item.h2}</h2>
-        `;    
-        const cajaOne = document.createElement('div');
-        cajaOne.innerHTML = `
-       
-                    <img class= "imgPeople efects" src="${item.imgSrc}" alt="">
-        `;
-        const cajatwo = document.createElement('div');
-        item.itemBox.forEach(boxItem => {
-            cajatwo.innerHTML += `
-                <div class= "boxy ${boxItem.bgColor} efectsBoxy">
-                    <p class = "boxNumero ${boxItem.tColor}" >${boxItem.n}</p>
-                    <p class="boxText">${boxItem.p}</p>
-                </div>
+            threeSection.innerHTML += `
+                    <h2 class="efects t-48">${item.h2}</h2>
+                    <img  src="${item.img}" alt=""/>
             `;
-        
-        });
-        threeSection.appendChild(cajaCero);
-        threeSection.appendChild(cajaOne);
-        threeSection.appendChild(cajatwo);
         });
 
         //section four
         const fourSection = document.querySelector('.fourSection');
         data.fourSection.forEach(item => {
-        const div = document.createElement('div');
-        div.innerHTML = `
-                    <img class="imgCrad efects" src="${item.imgSvg}" alt="">
-                    <div class="boxCard ${item.bgColor}">
-                        <h2>${item.h2}</h2>
-                        <p>${item.p}</p>
-                        <img class= "imgPhone efects" src="${item.imgPhone}" alt="">
-                    </div>
-        `;
-        
-            fourSection.appendChild(div);
-
+        const cajaCero = document.createElement('div');
+        cajaCero.innerHTML =`
+                    <h2 class="efects">${item.h2}</h2>
+        `;    
+        const cajaOne = document.createElement('div');
+        item.itemBox.forEach(boxItem => {
+            cajaOne.innerHTML += `
+                <div class= "boxy ${boxItem.bgColor} efectsBoxy">
+                    <p class = "boxNumero ${boxItem.tColor}" >${boxItem.t}</p>
+                    <p class="boxText">${boxItem.p}</p>
+                </div>
+            `;
         
         });
-        
-        //section five
+        fourSection.appendChild(cajaCero);
+        fourSection.appendChild(cajaOne);
+        });
+
+        //section five objetivos
         const fiveSection = document.querySelector('.fiveSection');
         data.fiveSection.forEach(item => {
+        const cajaCero = document.createElement('div');
+        cajaCero.innerHTML = `
+                    <h2 class="efects t-48">${item.h2}</h2>
+        `;
+        const cajaOne = document.createElement('div');
+        item.itemBox.forEach(boxItem => {
+            cajaOne.innerHTML += `
+                <div class= " efectsBoxy">
+                <img  src="${boxItem.img}" alt=""/>
+                    <p>${boxItem.p}</p>
+                </div>
+            `;
+        
+        });
+        fiveSection.appendChild(cajaCero);
+        fiveSection.appendChild(cajaOne)
+        });
+        
+        //section eleven
+        const tenSection = document.querySelector('.tenSection');
+        data.tenSection.forEach(item => {
         const div = document.createElement('div');
         div.innerHTML = `
                     <div class="itemsOne">
-                        <p>${item.p}</p>
-                        <img class="logoSlogan" src="${item.imgSrc}" alt="Espacio">
+                            <img class="uno logoSlogan" src="${item.imgSrc}" alt="Espacio">
+                            <div class="dos redes">
+                                <img src="${item.fbSvg}" alt=""></img>
+                                <img src="${item.twSvg}" alt=""></img>
+                                <img src="${item.intSvg}" alt=""></img>
+                            </div>
+                            <p class="tres">${item.p}</p>
                     </div>
         `;
                 
-            fiveSection.appendChild(div);
+            tenSection.appendChild(div);
         });
 })  
 
